921b8ec2fb83ff2b730ebde9a6d6ff169af89492
Tommy Vo <nghia.t.vo.0509@gmail.com>
2016-04-05 21:44:59 -0400

    refine stemming

--- feature_extract.py
+++ feature_extract.py
@@ -33,35 +33,24 @@ def get_rakeweight_data(doc):
   # also need to keep a mapping back from stemmed-tagged version to un-stemmed but lemmatized
   mapping_back = {}
   # maybe use a flag here instead of True
-  if False:
-    sentences = lemmatize_sen(sentences)
-    sentences, mapping_back = stem_sen(sentences)
+  all_tokens = []
+  if True:
+    sentences, all_tokens, mapping_back = stem_sen(sentences)
+    all_tokens = sorted(list(set(all_tokens)))
   else:
     # if we don't want to use the transformation, just keep mapping_back be a mirror from each token to itself
     for sent in sentences:
       tok_list = word_tokenize(sent)
       for tok in tok_list:
         mapping_back[tok] = tok
+        all_tokens.append(tok)
+    sentences = [word_tokenize(sent) for sent in sentences]
+    all_tokens = sorted(list(set(all_tokens)))
 
 
-  # tokenize words in sentences
-  sentences = [word_tokenize(sent) for sent in sentences]
-
-  # TODO: maybe put a flag in featrue extract for all of these options
   # remove stopwords
-  # sentences = [list(t for t in sent if ( (len(t) > 1) and (t.lower()
-    #  not in stopwords.words('english')) )) for sent in sentences]
+  sentences = [list(t for t in sent if ( (len(t) > 1) and (t.lower()not in stopwords.words('english')) )) for sent in sentences]
 
-  # stem tokens
-  # dont stem for now
-#  stemmer = EnglishStemmer()
-#  sentences = [list(stemmer.stem(t) for t in sent) for sent in sentences]
-
-  # get list of all tokens
-  all_tokens = [t for sent in sentences for t in sent]
-
-  # remove duplicates and sort
-  all_tokens = sorted(list(set(all_tokens)))
 
   # ---- replace rakeweight with differenct weighting scheme ----
 
@@ -113,35 +102,31 @@ def remove_non_nva_sen(list_sentences):
     res_list.append(tmp_list)
   return res_list
 
-
-# a bad thing that can happen when lemmatize token is that we lose plurality information
-def lemmatize_sen(list_sentences):
-  lem = WordNetLemmatizer()
-  res_list = []
-  for sent in list_sentences:
-    tmp_list = ''
-    tok_list = word_tokenize(sent)
-    for tok in tok_list:
-      tok = lem.lemmatize(tok)
-      tmp_list = tmp_list + tok + ' '
-    res_list.append(tmp_list)
-  return res_list
-
 def stem_sen(list_sentences):
   stemmer = EnglishStemmer()
   # map back should be a dict with words, 
   # each word map to 3 version: noun, adj, verb,
   # and each version is a list of pair
+  lem = WordNetLemmatizer()
   mapping_back = {}
   res_list = []
+  res_sen = []
   stemmer = EnglishStemmer()
 
   # of course we want to return a list of sentences back as well
   for sent in list_sentences:
-    tmp_list = ''
+    tmp_list = []
     tok_list = word_tokenize(sent)
     tok_pos = nltk.pos_tag(tok_list)
     for tok,pos in tok_pos:
+      if (tok.lower() in stopwords.words('english')):
+        continue
+      if len(tok) == 1:
+        continue
+      tok = lem.lemmatize(tok)
+      pos = pos[:2]
+      if ('NN' not in pos) and ('JJ' not in pos) and ('VB' not in pos):
+        continue
       stem_tok = stemmer.stem(tok)
       if (stem_tok not in mapping_back):
         mapping_back[stem_tok] = {}
@@ -153,11 +138,8 @@ def stem_sen(list_sentences):
         mapping_back[stem_tok][pos][tok] = 1
       else:
         mapping_back[stem_tok][pos][tok] += 1
-
-      # assume NN, JJ, VB always in the front of pos
-      pos = pos[:2]
-      tmp_list = tmp_list + stem_tok + '-' + pos + ' '
-    res_list.append(tmp_list)
+      tmp_list.append(stem_tok + '-' + pos)
+    res_sen.append(tmp_list)
   res_map = {}
 
   # do the second run through to find the most frequent - mapping
@@ -167,30 +149,5 @@ def stem_sen(list_sentences):
       # find the most frequently, unstemmed word correspond to the stemmer + tagged 
       most_freq = max(mapping_back[tok][pos], key = mapping_back[tok][pos].get)
       res_map[tmp_tok] = most_freq
-
-  return res_list, res_map
-# THE REMAINING FUNCTIONS MIGHT NOT BE USEFUL
-# counts how many times each token is used as noun/verb/adj...
-# not sure if this is really useful
-def pos_count(list_sentences, tokens):
-  pos_dict = {}
-  for tok in tokens:
-    pos_dict[tok] = {'NN':0, 'VB':0, 'JJ':0}
-  for sent in list_sentences:
-    tokenized = word_tokenize(sent)
-    pos_tok = nltk.pos_tag(tokenized)
-    for tok, pos in pos_tok:
-      if 'NN' in pos:
-        pos_tok[tok]['NN'] +=1
-      if 'JJ' in pos:
-        pos_tok[tok]['JJ'] += 1
-      if 'VB' in pos:
-        pos_tok[tok]['VB'] += 1
-  return pos_dict
-
-def pos_reweight(noun_weight, verb_weight, adj_weight, data, pos_dict):
-  for tok in tokens:
-    # find which position tok is used most
-    max_pos = max(pos_dict, key=pos_dict.get)
-    # readjust weight based on max_pos, but need to agree on the form of data
-    # .. a few lines of code to finish this function! should be easy
+      res_list.append(tmp_tok)
+  return res_sen, res_list, res_map
