ef01bc6668adf41e7f857c388c2f8cbbf8bf3c7c
haoyuliu <haoyuliu@ggbl2570p05.engin.umich.edu>
2016-04-20 17:10:49 -0400

    clean up svd

--- svd.py
+++ svd.py
@@ -18,6 +18,7 @@ from postprocess import get_keyphrase_weights
 
 #interfaces:
 #svd function, out put the principal component
+#performe svd algorithm and extract num_of_comp principal components
 def svd_mat(mat, vocalst, map_back, C, num_of_comp = 3):
 
 	mat = np.transpose(mat)
@@ -39,40 +40,18 @@ def svd_mat(mat, vocalst, map_back, C, num_of_comp = 3):
 		degree = sum(C[token].values())
 		keyword_weights[keyword] += float(degree)
 		rlst.append(keyword)
-	'''
-	u1 = U[:, 0]
-	u1x = np.absolute(u1)
-	sortidx = list(np.argsort(u1x))
-	sortidx.reverse()
-	print ("first level: with score: " + str(s[0]))
-	for i in range(0, 10):
-		print vocalst[sortidx[i]]
-
-
-
-	u2 = U[:,1]
-	sortidx2 = list(np.argsort(u2))
-	sortidx2.reverse()
-	print ("second level: with socre: " + str(s[1]))
-	for i in range(0, 10):
-		print vocalst[sortidx2[i]]
-
-	for i in range(0, 20):
-		print s[i]
-	return rlst
-	'''
 	return rlst, keyword_weights
 
 #process the file get a list of sentences
 def process_file(docstr):
-
-	#f = open(filename)
-	#s = f.read()
+#input is the string of the document
 	s = docstr
 	return get_rakeweight_data(s)
 
 
 def make_mat(lst):
+#construct matrix over a list of sentences
+#which is not used in evaluation, used for developing purpose
 	outlst = []
 	vocabulary = dict()
 	N = len(lst)
@@ -109,8 +88,11 @@ def make_mat(lst):
 	return mat, vocalst
 
 def svd(filename, num_of_comp = 3):
+#interface with the evaluation script
+#filename is the input document string as in process_file function
+#get document matrix and associated data structures
 	tokens, data, map_back, postprocess_sentences, C = process_file(filename)
-#	mat, vocalst = make_mat(lst)
+#perform svd
 	keywords, keyword_weights = svd_mat(data, tokens, map_back, C, num_of_comp)
   # combine into multiple keywords
 	keyphrases,keyphrase_freq = get_keyphrases(keywords, postprocess_sentences)
